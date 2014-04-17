8ad0299ba93aa1fa81156a8e93d802232d868872
Jesse Roberts <isaiah@isaiahjroberts.com>
2014-04-16 22:52:53 -0600

    add getip, unittest

--- pyflare.py
+++ pyflare.py
@@ -1,17 +1,30 @@
 #!/usr/bin/env python
 
 import unittest
+import requests
 
 
+class PyFlare(object):
+    
+    def getip(self):
+        ip = requests.get('http://icanhazip.com').text.rstrip('\n')
+        return ip
+    
+    def validpost(self):
+        pass
 
 
 
 # ------ Unittesting ------ #
 
-class test_PyFlare(unittest):
+class test_PyFlare(unittest.TestCase):
 
     def test_getip(self):
-        pass
+        self.assertEqual(PyFlare().getip(), '71.209.47.192')
+        
 
     def test_validpost(self):
         pass
+
+if __name__ == '__main__':
+    unittest.main()
