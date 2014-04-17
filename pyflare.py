08966909699608e09f8c9667c32684407c4b393f
Jesse Roberts <isaiah@isaiahjroberts.com>
2014-04-16 21:57:38 -0600

    stub unittest

---
+++ pyflare.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import unittest
+
+
+
+
+
+# ------ Unittesting ------ #
+
+class test_PyFlare(unittest):
+
+    def test_getip(self):
+        pass
+
+    def test_validpost(self):
+        pass
