e08232c3d0bc36180c880b321834b402e6ec4529
Jesse Roberts <isaiah@isaiahjroberts.com>
2014-04-18 23:10:55 -0600

    add rec_edit, stub

--- pyflare.py
+++ pyflare.py
@@ -6,13 +6,39 @@ import requests
 
 class PyFlare(object):
     
+    def __init__(self):
+        self.tkn = None
+        self.email = None
+        self.a = None # API uses this for determining the request type
+        self.url = 'https://www.cloudflare.com/api_json.html'
+        self.post = {}
+    
     def getip(self):
         ip = requests.get('http://icanhazip.com').text.rstrip('\n')
         return ip
     
-    def getapiendpoint(self):
-        api = 'https://www.cloudflare.com/api_json.html'
+    def loadcfg(self, cfg=None):
+        '''Opens config file and returns cfg for key passed in'''
+        cfg = cfg
+        pass
 
+    def rec_edit(self):
+        post = {
+        'a': 'rec_edit',
+        'tkn': self.loadcfg(cfg='api_key'),
+        'id': '', #dns record id
+        'email': self.loadcfg(cfg='email'),
+        'z': self.loadcfg(cfg='zone'),
+        'type': 'A',
+        'name': self.loadcfg(cfg='record_name'),
+        'content': self.getip(),
+        'service_mode': '1',
+        'ttl': '1'
+        }
+        return post
+    
+    def rec_load_all(self):
+        
 
 
 # ------ Unittesting ------ #
